351977d19ffa1c496dd915c4c30e80dab137e1a1
bogdankvlv <70582969+bogdankvlv@users.noreply.github.com>
2024-06-19 20:29:11 -0400

    Second commit

---
+++ src/Components/Middle.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Segment, Image } from 'semantic-ui-react';
+
+
+const MiddleSection = () => {
+  return (
+    <Segment
+      style={{
+        height: '100vh',
+        backgroundImage: `url(${require('E:/Documents/myap/weatherapp/src/Components/images/aydobweather.png')})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      
+    </Segment>
+  );
+};
+
+export default MiddleSection;
